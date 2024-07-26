07a2e1aa24102c85c2b080aea93a3a35653e23e3
Krowd <kjrahu77@gmail.com>
2024-07-27 00:14:49 +0530

    1st

--- src/components/FirstPage.jsx
+++ src/components/FirstPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Box, Button, FormControl, FormLabel, Input, Heading, Stack } from '@chakra-ui/react';
+import { Box, Button, FormControl, FormLabel, Input, Heading, VStack } from '@chakra-ui/react';
 import busData from './bus.json'; // Adjust the path if needed
 import './FirstPage.css'; // Import the CSS file
 
@@ -25,45 +25,44 @@ const FirstPage = () => {
 
   return (
     <div className="fullscreen-container">
-      <Box p={4} className="form-container">
+      <Box p={4} className="form-container" width="100%" maxWidth="500px">
         <Heading mb={6}>Search for Buses</Heading>
-        <form onSubmit={handleSubmit}>
-          <Stack spacing={4}>
-            <FormControl>
-              <FormLabel htmlFor="from">From:</FormLabel>
-              <Input
-                id="from"
-                name="from"
-                value={formData.from}
-                onChange={handleChange}
-                required
-              />
-            </FormControl>
-            <FormControl>
-              <FormLabel htmlFor="to">To:</FormLabel>
-              <Input
-                id="to"
-                name="to"
-                value={formData.to}
-                onChange={handleChange}
-                required
-              />
-            </FormControl>
-            <FormControl>
-              <FormLabel htmlFor="time">Time:</FormLabel>
-              <Input
-                id="time"
-                name="time"
-                value={formData.time}
-                onChange={handleChange}
-                required
-              />
-            </FormControl>
-            <Button type="submit" mt={4}>
-              Search
-            </Button>
-          </Stack>
-        </form>
+        <VStack as="form" onSubmit={handleSubmit} spacing={4}>
+          {/* Wrap form controls in VStack for vertical stacking with spacing */}
+          <FormControl>
+            <FormLabel htmlFor="from">From:</FormLabel>
+            <Input
+              id="from"
+              name="from"
+              value={formData.from}
+              onChange={handleChange}
+              required
+            />
+          </FormControl>
+          <FormControl>
+            <FormLabel htmlFor="to">To:</FormLabel>
+            <Input
+              id="to"
+              name="to"
+              value={formData.to}
+              onChange={handleChange}
+              required
+            />
+          </FormControl>
+          <FormControl>
+            <FormLabel htmlFor="time">Time:</FormLabel>
+            <Input
+              id="time"
+              name="time"
+              value={formData.time}
+              onChange={handleChange}
+              required
+            />
+          </FormControl>
+          <Button type="submit" mt={4}>
+            Search
+          </Button>
+        </VStack>
       </Box>
     </div>
   );
