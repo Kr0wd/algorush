8fb873a787ffd60109f1862a38f4064813e08b9d
Krowd <kjrahu77@gmail.com>
2024-07-26 22:40:17 +0530

    1st

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
-// src/App.jsx
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { ChakraProvider } from '@chakra-ui/react'; // Import ChakraProvider
 import FirstPage from './components/FirstPage';
 import SecondPage from './components/SecondPage';
 import ThirdPage from './components/ThirdPage';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<FirstPage />} />
-        <Route path="/second" element={<SecondPage />} />
-        <Route path="/third" element={<ThirdPage />} />
-      </Routes>
-    </Router>
+    <ChakraProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<FirstPage />} />
+          <Route path="/second" element={<SecondPage />} />
+          <Route path="/third" element={<ThirdPage />} />
+        </Routes>
+      </Router>
+    </ChakraProvider>
   );
 }
 
